711e00e936cb2a3dd7ba89cbd2f9c6a0c9ec2d9a
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-09-26 00:10:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,22 +21,36 @@ class Song
       @@count
     end
     
-    def artists
-      @@artists
+    def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+     genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
     end
-    
-    def genres
-      @@genres
+    genre_count
+  end
+  
+   def self.artist_count
+     artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
     end
-    
-    def genre_count
-      
-    end
-    
-    def artist_count
-      
-    end
-    
+    artist_count
   end
   
 end
