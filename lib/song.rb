cda8e80a5337fcbdf540af5b10123a444326510f
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-09-26 00:22:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,7 @@ class Song
   end
   
   def self.genre_count
-     genre_count = {}
+    genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
         genre_count[genre] += 1
@@ -37,8 +37,8 @@ class Song
     genre_count
   end
   
-   def self.artist_count
-     artist_count = {}
+  def self.artist_count
+    artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
         artist_count[artist] += 1
